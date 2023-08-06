6247c028f2d235566fe59f76aacf0d7692d89bb1
Qudus Qazeem <qudusqazeem@gmail.com>
2023-08-06 10:29:34 +0100

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Qazeem Abiodun on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
